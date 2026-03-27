7ba2729666edc44e14a0d39543a0855ba68eb511
Siego Loop <128708756+SiegoLoop@users.noreply.github.com>
2026-03-27 21:48:41 +0100

    Release version 1.8.0 - Cancel processing & LibreOffice support

--- docs/js/download.js
+++ docs/js/download.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       // Update UI
       statusEl.textContent = "Your payment was verified!";
       downloadLink.href = url;
-      downloadLink.download = "Floxtop-v1.7.0.zip";
+      downloadLink.download = "Floxtop-v1.8.0.zip";
       downloadLink.style.display = "inline-block";
       downloadLink.textContent = "Download Floxtop";
       
