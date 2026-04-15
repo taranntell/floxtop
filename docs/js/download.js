45cf26e3bf1369996cc242a0f90592fcff2461b0
Siego Loop <128708756+SiegoLoop@users.noreply.github.com>
2026-04-15 15:26:18 +0200

    Bump v1.9.0 release

--- docs/js/download.js
+++ docs/js/download.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       // Update UI
       statusEl.textContent = "Your payment was verified!";
       downloadLink.href = url;
-      downloadLink.download = "Floxtop-v1.8.0.zip";
+      downloadLink.download = "Floxtop-v1.9.0.zip";
       downloadLink.style.display = "inline-block";
       downloadLink.textContent = "Download Floxtop";
       
